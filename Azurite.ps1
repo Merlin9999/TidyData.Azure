12ec0db137e2a934eb6fd75217f608578154e728
Marc Behnke <marc.behnke@gmail.com>
2025-07-12 18:30:39 -0500

    Added script to run azurite

---
+++ Azurite.ps1
@@ -0,0 +1,5 @@
+#start -FilePath "powershell" -WorkingDirectory ".\.Azurite" -ArgumentList "azurite --location .\.Azurite --debug .\.Azurite\debug.log"
+
+
+start -FilePath "powershell" -ArgumentList "azurite --location .\.Azurite --debug .\.Azurite\debug.log"
+
