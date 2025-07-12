18765f4e90024d2834421b01fc4bf91951c4d878
Merlin9999 <null>
2025-07-12 21:33:05 +0000

    🔄 created local 'Src/TidyData.Azure.Tests/_Shared-Synced/' from remote 'Src/TidyData.Tests/_Shared-Synced/'

---
+++ Src/TidyData.Azure.Tests/_Shared-Synced/TestModel/Cmd/InsertTestDocCommand.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+
+// ReSharper disable CheckNamespace
+namespace TidyData.Tests._Shared_Synced.TestModel.Cmd;
+
+public class InsertTestDocCommand : ICommand<TestDataModel>
+{
+    private readonly TestDocument _docToInsert;
+
+    public InsertTestDocCommand(TestDocument docToInsert)
+    {
+        this._docToInsert = docToInsert;
+    }
+
+    public void Execute(TestDataModel model, CollectionWrapperFactory factory)
+    {
+        factory.Get(model, x => x.Docs)
+            .Insert(this._docToInsert);
+    }
+}
---
+++ Src/TidyData.Azure.Tests/_Shared-Synced/TestModel/Cmd/SoftDeleteTestDocCommand.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+
+// ReSharper disable CheckNamespace
+namespace TidyData.Tests._Shared_Synced.TestModel.Cmd;
+
+public class SoftDeleteTestDocCommand : ICommand<TestDataModel>
+{
+    private readonly Guid _idOfDocToDelete;
+
+    public SoftDeleteTestDocCommand(Guid idOfDocToDelete)
+    {
+        this._idOfDocToDelete = idOfDocToDelete;
+    }
+
+    public void Execute(TestDataModel model, CollectionWrapperFactory factory)
+    {
+        factory.Get(model, x => x.Docs)
+            .SoftDelete(this._idOfDocToDelete);
+    }
+}
